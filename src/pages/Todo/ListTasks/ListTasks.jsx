8b67371123a4613e4172ac77863f72dee5cc6b26
duong1801 <duongx1801@gmail.com>
2024-05-13 11:46:00 +0700

    update codde

--- src/pages/Todo/ListTasks/ListTasks.jsx
+++ src/pages/Todo/ListTasks/ListTasks.jsx
@@ -3,7 +3,7 @@
 import Task from "./Task/Task"
 
 function ListTasks({ tasks }) {
-	return tasks.map((task) => <Task task={task} key={task._id} />)
+	return tasks?.map((task) => <Task task={task} key={task._id} />)
 }
 
 export default ListTasks
