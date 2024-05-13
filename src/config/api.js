8d472cdd03324e9d9d3947667ed94eed7685b0aa
duong1801 <duongx1801@gmail.com>
2024-05-13 11:49:07 +0700

    update codde

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 /** @format */
 const config = {
 	API_ROOT: "https://api-todo-ebon.vercel.app/api/v1",
-	"X-Api-Key": "2c9afd83c1a141afbbcb83a4d4b82a35",
+	"X-Api-Key": "6ba733917e944a00b29dea5c3cad74e9",
 }
 export default config
